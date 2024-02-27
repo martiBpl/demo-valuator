1664e6fb1635376a19d003528e441ce278a056a5
Marcin Bawtrol <marcin.bawtrol@kevin.eu>
2024-02-27 13:59:29 +0100

    init commit

---
+++ src/main/java/demo/inpost/valuator/ValuatorApplication.java
@@ -0,0 +1,13 @@
+package demo.inpost.valuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/inpost/valuator/ValuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.inpost.valuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
